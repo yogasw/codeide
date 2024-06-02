d190f3346ddcbd4b1898cbf9eab61cf7acf616c4
Yoga Setiawan <yogainformatika@gmail.com>
2024-06-03 06:23:11 +0700

    init

--- main.go
+++ main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "",
+	Short: "Simplifying VS Code for Open Specific Programming Languages",
+	Run:   detectLanguage,
+}
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+	}
+}
+
+func detectLanguage(cmd *cobra.Command, args []string) {
+
+}
